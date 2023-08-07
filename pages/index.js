a9825292cec071057dd04aae017c4f9619cbc66b
GustavoPortox <49151600+GustavoPortox@users.noreply.github.com>
2023-08-07 16:14:15 +0000

    adicionado comentários sobre as demais aulas

--- pages/index.js
+++ pages/index.js
@@ -2,3 +2,10 @@ function Home() {
     return <h1>Teste de deploy em outro terminal</h1>
 }
 export default Home;
+
+// trabalhar pouco e ganhar muito - planejar muito faz com que a execução seja 'pouca'
+// calculando o saldo das coisas
+// 1 - ser lembrado individualmente (menor custo de producao / energia - aquecimento / o tempo para ver algo acontecendo
+// 2 - ser lembrado em grupo
+// 3 - expandir conhecimento
+// 4 - gerar métricas
