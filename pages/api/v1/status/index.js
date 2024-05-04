7db3c65d155d47a5f4f5de70829fcc4a01a1f89d
GustavoPortox <49151600+GustavoPortox@users.noreply.github.com>
2024-05-04 22:24:30 +0000

    add version, max_connection and opened_connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Api de verdade!" });
+  const updateAt = new Date().toISOString();
+
+  const databaseResult = await database.query("SHOW server_version;");
+
+  const databaseValue = databaseResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].length;
+  console.log(databaseMaxConnectionsValue);
+  console.log(databaseValue);
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdated = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdated).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
